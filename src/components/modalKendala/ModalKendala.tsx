270ff692e638effd70abe19302b9507b6d5d1536
supriadi <supriadiadi1001@gmail.com>
2023-05-31 00:26:47 +0700

    update

--- src/components/modalKendala/ModalKendala.tsx
+++ src/components/modalKendala/ModalKendala.tsx
@@ -1,25 +1,234 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { ScrollView, TouchableOpacity, Modal, StyleSheet, Text, View } from 'react-native';
 
-const ModalKendala = ({ modalKendalaVisible, setModalKendala }) => {
-  const [k1, setK1] = useState(0);
-  const [k2, setK2] = useState(0);
-  const [k3, setK3] = useState(0);
-  const [k4, setK4] = useState(0);
-  const [k5, setK5] = useState(0);
-  const [k6, setK6] = useState(0);
-  const [k7, setK7] = useState(0);
-  const [k8, setK8] = useState(0);
-  const [k9, setK9] = useState(0);
-  const [k10, setK10] = useState(0);
-  const [k11, setK11] = useState(0);
-  const [k12, setK12] = useState(0);
-  const [k13, setK13] = useState(0);
+const ModalKendala = ({ modalKendalaVisible, setModalKendala, handleKerusakan }) => {
+  const [selectedButtons, setSelectedButtons] = useState([]);
+  const [jenisKerusakan, setJenisKerusakan] = useState([]);
+  const [persentaseKerusakan, setPersentaseKerusakan] = useState(0);
+
+  const handleButtonPress = (buttonIndex) => {
+    if (selectedButtons.includes(buttonIndex)) {
+      setSelectedButtons(selectedButtons.filter((index) => index !== buttonIndex));
+    } else {
+      if (selectedButtons.length < 3) {
+        setSelectedButtons([...selectedButtons, buttonIndex]);
+      }
+    }
+  };
+
+  useEffect(() => {
+    const checkKerusakan = () => {
+      let totalPersentase = 0;
+      const kerusakan = [];
+      // pertama
+      if (
+        selectedButtons.includes(0) &&
+        selectedButtons.includes(1) &&
+        selectedButtons.includes(7)
+      ) {
+        // kerusakan.push( {diagnosis:'Tombol On/Off tidak tertekan');
+        kerusakan.push({ diagnosis: 'Tombol On/Off tidak tertekan', percentage: 100 });
+        // totalPersentase += 100;
+      } else if (
+        (selectedButtons.includes(0) && selectedButtons.includes(1)) ||
+        (selectedButtons.includes(0) && selectedButtons.includes(7)) ||
+        (selectedButtons.includes(1) && selectedButtons.includes(7))
+      ) {
+        kerusakan.push({
+          diagnosis: 'Kerusakan Mungkin terletak pada Tombol On/Off tidak tertekan',
+          percentage: 77,
+        });
+      } else if (selectedButtons.length === 1) {
+        kerusakan.push({
+          diagnosis: 'Kerusakan belum bisa dipastikan',
+          percentage: 33,
+        });
+      }
+      if (
+        selectedButtons.includes(0) &&
+        selectedButtons.includes(1) &&
+        selectedButtons.includes(11)
+      ) {
+        kerusakan.push({
+          diagnosis: 'Sumber kabel konslet',
+          percentage: 100,
+        });
+      } else if (
+        (selectedButtons.includes(0) && selectedButtons.includes(1)) ||
+        (selectedButtons.includes(0) && selectedButtons.includes(11)) ||
+        (selectedButtons.includes(1) && selectedButtons.includes(11))
+      ) {
+        kerusakan.push({
+          diagnosis: 'Kerusakan Mungkin terletak Sumber kabel konslet',
+          percentage: 77,
+        });
+      } else if (selectedButtons.length === 1) {
+        kerusakan.push({
+          diagnosis: 'Kerusakan belum bisa dipastikan',
+          percentage: 33,
+        });
+      }
+
+      if (
+        selectedButtons.includes(0) &&
+        selectedButtons.includes(2) &&
+        selectedButtons.includes(9)
+      ) {
+        kerusakan.push({ diagnosis: 'Mesin panas', percentage: 100 });
+        totalPersentase += 100;
+      } else if (
+        (selectedButtons.includes(0) && selectedButtons.includes(2)) ||
+        (selectedButtons.includes(0) && selectedButtons.includes(9)) ||
+        (selectedButtons.includes(2) && selectedButtons.includes(9))
+      ) {
+        kerusakan.push({ diagnosis: 'Kerusakan Mungkin karena Mesin panas', percentage: 77 });
+      } else if (selectedButtons.length === 1) {
+        kerusakan.push({ diagnosis: 'Kerusakan belum bisa dipastikan', percentage: 33 });
+        totalPersentase += 0;
+      }
+
+      if (
+        selectedButtons.includes(1) &&
+        selectedButtons.includes(2) &&
+        selectedButtons.includes(8)
+      ) {
+        kerusakan.push({ diagnosis: 'Biji kopi kepenuhan atau terlalu padat', percentage: 100 });
+        totalPersentase += 100;
+      } else if (
+        (selectedButtons.includes(1) && selectedButtons.includes(2)) ||
+        (selectedButtons.includes(1) && selectedButtons.includes(8)) ||
+        (selectedButtons.includes(2) && selectedButtons.includes(8))
+      ) {
+        kerusakan.push({
+          diagnosis: 'Kerusakan Mungkin Karena Biji kopi kepenuhan atau terlalu padat',
+          percentage: 77,
+        });
+        totalPersentase += 77;
+      } else if (selectedButtons.length === 1) {
+        kerusakan.push({ diagnosis: 'Kerusakan belum bisa dipastikan', percentage: 33 });
+        totalPersentase += 0;
+      }
+
+      if (
+        selectedButtons.includes(1) &&
+        selectedButtons.includes(2) &&
+        selectedButtons.includes(9)
+      ) {
+        kerusakan.push({ diagnosis: 'Pompa rusak / Air pada pemasok air habis', percentage: 100 });
+        totalPersentase += 100;
+      } else if (
+        (selectedButtons.includes(1) && selectedButtons.includes(2)) ||
+        (selectedButtons.includes(1) && selectedButtons.includes(9)) ||
+        (selectedButtons.includes(2) && selectedButtons.includes(9))
+      ) {
+        kerusakan.push({
+          diagnosis: 'Kerusakan Mungkin Karena Pompa rusak / Air pada pemasok air habis',
+          percentage: 77,
+        });
+        totalPersentase += 77;
+      } else if (selectedButtons.length === 1) {
+        kerusakan.push({ diagnosis: 'Kerusakan belum bisa dipastikan', percentage: 33 });
+        totalPersentase += 0;
+      }
+
+      if (
+        selectedButtons.includes(1) &&
+        selectedButtons.includes(2) &&
+        selectedButtons.includes(12)
+      ) {
+        kerusakan.push({ diagnosis: 'Saluran air tersumbat', percentage: 100 });
+        totalPersentase += 100;
+      } else if (
+        (selectedButtons.includes(1) && selectedButtons.includes(2)) ||
+        (selectedButtons.includes(1) && selectedButtons.includes(12)) ||
+        (selectedButtons.includes(2) && selectedButtons.includes(12))
+      ) {
+        kerusakan.push({
+          diagnosis: 'Kerusakan Mungkin Karena Saluran air tersumbat',
+          percentage: 77,
+        });
+        totalPersentase += 77;
+      } else if (selectedButtons.length === 1) {
+        kerusakan.push({ diagnosis: 'Kerusakan belum bisa dipastikan', percentage: 33 });
+        totalPersentase += 0;
+      }
+
+      if (
+        selectedButtons.includes(3) &&
+        selectedButtons.includes(8) &&
+        selectedButtons.includes(11)
+      ) {
+        kerusakan.push({ diagnosis: 'Gilingan kopi yang masih kasar', percentage: 100 });
+        totalPersentase += 100;
+      } else if (
+        (selectedButtons.includes(3) && selectedButtons.includes(8)) ||
+        (selectedButtons.includes(3) && selectedButtons.includes(11)) ||
+        (selectedButtons.includes(8) && selectedButtons.includes(11))
+      ) {
+        kerusakan.push({
+          diagnosis: 'Kerusakan Mungkin Karena Gilingan kopi yang masih kasar',
+          percentage: 77,
+        });
+        totalPersentase += 77;
+      } else if (selectedButtons.length === 1) {
+        kerusakan.push({ diagnosis: 'Kerusakan belum bisa dipastikan', percentage: 33 });
+        totalPersentase += 0;
+      }
+
+      if (
+        selectedButtons.includes(4) &&
+        selectedButtons.includes(5) &&
+        selectedButtons.includes(10)
+      ) {
+        kerusakan.push({ diagnosis: 'Filter sudah tua', percentage: 100 });
+        totalPersentase += 100;
+      } else if (
+        (selectedButtons.includes(4) && selectedButtons.includes(5)) ||
+        (selectedButtons.includes(4) && selectedButtons.includes(10)) ||
+        (selectedButtons.includes(5) && selectedButtons.includes(10))
+      ) {
+        kerusakan.push({ diagnosis: 'Kerusakan Mungkin Karena Filter sudah tua', percentage: 77 });
+        totalPersentase += 77;
+      } else if (selectedButtons.length === 1) {
+        kerusakan.push({ diagnosis: 'Kerusakan belum bisa dipastikan', percentage: 33 });
+        totalPersentase += 0;
+      }
+
+      if (
+        selectedButtons.includes(6) &&
+        selectedButtons.includes(7) &&
+        selectedButtons.includes(9)
+      ) {
+        kerusakan.push({ diagnosis: 'Selang bocor', percentage: 100 });
+        totalPersentase += 100;
+      } else if (
+        (selectedButtons.includes(6) && selectedButtons.includes(7)) ||
+        (selectedButtons.includes(4) && selectedButtons.includes(11)) ||
+        (selectedButtons.includes(6) && selectedButtons.includes(11))
+      ) {
+        kerusakan.push({ diagnosis: 'Kerusakan Mungkin Karena Filter sudah tua', percentage: 77 });
+        totalPersentase += 77;
+      } else if (selectedButtons.length === 1) {
+        kerusakan.push({ diagnosis: 'Kerusakan belum bisa dipastikan', percentage: 33 });
+        totalPersentase += 0;
+      }
+      setJenisKerusakan(kerusakan);
+      setPersentaseKerusakan(totalPersentase);
+    };
+
+    checkKerusakan();
+  }, [selectedButtons]);
 
   const handleKonfirmasi = () => {
+    if (jenisKerusakan) {
+      handleKerusakan(jenisKerusakan, persentaseKerusakan);
+    } else {
+      handleKerusakan('', 0);
+    }
     setModalKendala(false);
   };
 
+  console.log('selectedButtons', jenisKerusakan);
   return (
     <Modal
       animationType="slide"
@@ -35,119 +244,22 @@ const ModalKendala = ({ modalKendalaVisible, setModalKendala }) => {
           </View>
           <View style={{ gap: 8, height: 400, paddingVertical: 20 }}>
             <ScrollView>
-              <TouchableOpacity
-                style={k1 & 1 ? [styles.viewRow] : [styles.viewRow1]}
-                onPress={() => setK1((prevK1: number) => (prevK1 & 1 ? prevK1 - 1 : prevK1 + 1))}>
-                <Text style={k1 & 1 ? [styles.textChoice] : [styles.textChoice1]}>
-                  Mesin Tidak Menyala
-                </Text>
-              </TouchableOpacity>
-
-              <TouchableOpacity
-                style={k2 & 1 ? [styles.viewRow] : [styles.viewRow1]}
-                onPress={() => setK2((prevK2: number) => (prevK2 & 1 ? prevK2 - 1 : prevK2 + 1))}>
-                <Text style={k2 & 1 ? [styles.textChoice] : [styles.textChoice1]}>
-                  Mesin Tidak Mengeluarkan Kopi
-                </Text>
-              </TouchableOpacity>
-
-              <TouchableOpacity
-                style={k3 & 1 ? [styles.viewRow] : [styles.viewRow1]}
-                onPress={() => setK3((prevK3: number) => (prevK3 & 1 ? prevK3 - 1 : prevK3 + 1))}>
-                <Text style={k3 & 1 ? [styles.textChoice] : [styles.textChoice1]}>
-                  Mesin mengeluarkan kopi Namun Lambat
-                </Text>
-              </TouchableOpacity>
-
-              <TouchableOpacity
-                style={k4 & 1 ? [styles.viewRow] : [styles.viewRow1]}
-                onPress={() => setK4((prevK4: number) => (prevK4 & 1 ? prevK4 - 1 : prevK4 + 1))}>
-                <Text style={k4 & 1 ? [styles.textChoice] : [styles.textChoice1]}>
-                  Kopi Terlalu Cepat Keluar
-                </Text>
-              </TouchableOpacity>
-
-              <TouchableOpacity
-                style={k5 & 1 ? [styles.viewRow] : [styles.viewRow1]}
-                onPress={() => setK5((prevK5: number) => (prevK5 & 1 ? prevK5 - 1 : prevK5 + 1))}>
-                <Text style={k5 & 1 ? [styles.textChoice] : [styles.textChoice1]}>
-                  Kopi Bocor, Sehingga Butiran Kopi Keluar Di Portfilter
-                </Text>
-              </TouchableOpacity>
-
-              <TouchableOpacity
-                style={k6 & 1 ? [styles.viewRow] : [styles.viewRow1]}
-                onPress={() => setK6((prevK6: number) => (prevK6 & 1 ? prevK6 - 1 : prevK6 + 1))}>
-                <Text style={k6 & 1 ? [styles.textChoice] : [styles.textChoice1]}>
-                  Terdapat Genangan Air Kotor Pada Bagian Bawah Mesin
-                </Text>
-              </TouchableOpacity>
-
-              <TouchableOpacity
-                style={k7 & 1 ? [styles.viewRow] : [styles.viewRow1]}
-                onPress={() => setK7((prevK7: number) => (prevK7 & 1 ? prevK7 - 1 : prevK7 + 1))}>
-                <Text style={k7 & 1 ? [styles.textChoice] : [styles.textChoice1]}>
-                  Terdapat Genangan Air Bersih Pada Bagian Bawah Mesin
-                </Text>
-              </TouchableOpacity>
-
-              <TouchableOpacity
-                style={k8 & 1 ? [styles.viewRow] : [styles.viewRow1]}
-                onPress={() => setK8((prevK8: number) => (prevK8 & 1 ? prevK8 - 1 : prevK8 + 1))}>
-                <Text style={k8 & 1 ? [styles.textChoice] : [styles.textChoice1]}>
-                  Lampu Indikator Mesin Tidak Hidup
-                </Text>
-              </TouchableOpacity>
-
-              <TouchableOpacity
-                style={k9 & 1 ? [styles.viewRow] : [styles.viewRow1]}
-                onPress={() => setK9((prevK9: number) => (prevK9 & 1 ? prevK9 - 1 : prevK9 + 1))}>
-                <Text style={k9 & 1 ? [styles.textChoice] : [styles.textChoice1]}>
-                  Mesin Berbunyi Keras
-                </Text>
-              </TouchableOpacity>
-
-              <TouchableOpacity
-                style={k10 & 1 ? [styles.viewRow] : [styles.viewRow1]}
-                onPress={() =>
-                  setK10((prevK10: number) => (prevK10 & 1 ? prevK10 - 1 : prevK10 + 1))
-                }>
-                <Text style={k10 & 1 ? [styles.textChoice] : [styles.textChoice1]}>
-                  Air Tidak Keluar Saat Awal Mesin Dihidupkan
-                </Text>
-              </TouchableOpacity>
-
-              <TouchableOpacity
-                style={k11 & 1 ? [styles.viewRow] : [styles.viewRow1]}
-                onPress={() =>
-                  setK11((prevK11: number) => (prevK11 & 1 ? prevK11 - 1 : prevK11 + 1))
-                }>
-                <Text style={k11 & 1 ? [styles.textChoice] : [styles.textChoice1]}>
-                  Rasa Kopi Kurang Enak
-                </Text>
-              </TouchableOpacity>
-
-              <TouchableOpacity
-                style={k12 & 1 ? [styles.viewRow] : [styles.viewRow1]}
-                onPress={() =>
-                  setK12((prevK12: number) => (prevK12 & 1 ? prevK12 - 1 : prevK12 + 1))
-                }>
-                <Text style={k12 & 1 ? [styles.textChoice] : [styles.textChoice1]}>
-                  Terdapat Aroma Kabel Terbakar
-                </Text>
-              </TouchableOpacity>
-
-              <TouchableOpacity
-                style={k13 & 1 ? [styles.viewRow] : [styles.viewRow1]}
-                onPress={() =>
-                  setK13((prevK13: number) => (prevK13 & 1 ? prevK13 - 1 : prevK13 + 1))
-                }>
-                <Text style={k13 & 1 ? [styles.textChoice] : [styles.textChoice1]}>
-                  Terdapat Aroma Tidak Sedap
-                </Text>
-              </TouchableOpacity>
+              {buttonData.map((button, index) => (
+                <TouchableOpacity
+                  key={index}
+                  style={selectedButtons.includes(index) ? styles.viewRow : styles.viewRow1}
+                  onPress={() => handleButtonPress(index)}>
+                  <Text
+                    style={
+                      selectedButtons.includes(index) ? styles.textChoice : styles.textChoice1
+                    }>
+                    {index + 1}.{button.label}
+                  </Text>
+                </TouchableOpacity>
+              ))}
             </ScrollView>
           </View>
+
           <TouchableOpacity style={styles.button} onPress={handleKonfirmasi}>
             <Text style={styles.textKonfirmasi}>Konfirmasi</Text>
           </TouchableOpacity>
@@ -158,6 +270,22 @@ const ModalKendala = ({ modalKendalaVisible, setModalKendala }) => {
 };
 
 export default ModalKendala;
+
+const buttonData = [
+  { label: 'Mesin Tidak Menyala' },
+  { label: 'Mesin Tidak Mengeluarkan Kopi' },
+  { label: 'Mesin mengeluarkan kopi Namun Lambat' },
+  { label: 'Kopi Terlalu Cepat Keluar' },
+  { label: 'Kopi Bocor, Sehingga Butiran Kopi Keluar Di Portfilter' },
+  { label: 'Terdapat Genangan Air Kotor Pada Bagian Bawah Mesin' },
+  { label: 'Terdapat Genangan Air Bersih Pada Bagian Bawah Mesin' },
+  { label: 'Lampu Indikator Mesin Tidak Hidup' },
+  { label: 'Mesin Berbunyi Keras' },
+  { label: 'Air Tidak Keluar Saat Awal Mesin Dihidupkan' },
+  { label: 'Rasa Kopi Kurang Enak' },
+  { label: 'Terdapat Aroma Kabel Terbakar' },
+  { label: 'Terdapat Aroma Tidak Sedap' },
+];
 
 const styles = StyleSheet.create({
   textKonfirmasi: {
@@ -184,7 +312,6 @@ const styles = StyleSheet.create({
     marginVertical: 4,
     paddingHorizontal: 12,
     borderRadius: 4,
-
     backgroundColor: '#2D5BD1',
   },
   textChoice1: {
@@ -216,5 +343,18 @@ const styles = StyleSheet.create({
   text: {
     fontFamily: 'Poppins_SemiBold',
     fontSize: 14,
+  },
+  kerusakanContainer: {
+    marginTop: 20,
+    alignItems: 'center',
+  },
+  textKerusakan: {
+    fontFamily: 'Poppins_SemiBold',
+    fontSize: 16,
+  },
+  textPersentase: {
+    fontFamily: 'Poppins_Regular',
+    fontSize: 14,
+    marginTop: 8,
   },
 });
--- src/screens/Konsultasi.tsx
+++ src/screens/Konsultasi.tsx
@@ -1,73 +1,104 @@
 import { Icon } from '@rneui/base';
 import React, { useState } from 'react';
-import { StyleSheet, Text, View, Image, TouchableOpacity, ScrollView, Alert } from 'react-native';
+import { StyleSheet, Text, View, Image, TouchableOpacity, ScrollView } from 'react-native';
 
 import Layout from '../components/Layout';
 import ModalKendala from '../components/modalKendala/ModalKendala';
 
 const Konsultasi = (props: any) => {
   const [modalKendala, setModalKendala] = useState(false);
-
-  const handleKendala = () => {
-    alert('ini alert');
+  const [jenisKerusakan, setJenisKerusakan] = useState(undefined);
+  const [persentaseKerusakan, setPersentaseKerusakan] = useState(undefined);
+  const handleKerusakan = (jenisKerusakan, persentaseKerusakan) => {
+    setJenisKerusakan(jenisKerusakan);
+    setPersentaseKerusakan(persentaseKerusakan);
   };
-
+  console.log('jenisKerusakan', { jenisKerusakan });
   return (
-    <>
-      <Layout titleBar title="Konsultasi">
-        <View style={styles.contentView}>
-          <ScrollView>
-            <View
-              style={{
-                padding: 10,
-                alignItems: 'center',
-                justifyContent: 'center',
-                flexDirection: 'row',
-                flex: 1,
-              }}>
-              <Image
-                source={require('../../assets/image/Konsultasi.png')}
-                style={{ height: 201, width: 202 }}
-              />
+    <Layout titleBar title="Konsultasi">
+      <View style={styles.contentView}>
+        <ScrollView>
+          <View
+            style={{
+              padding: 10,
+              alignItems: 'center',
+              justifyContent: 'center',
+              flexDirection: 'row',
+              flex: 1,
+            }}>
+            <Image
+              source={require('../../assets/image/Konsultasi.png')}
+              style={{ height: 201, width: 202 }}
+            />
+          </View>
+
+          {/* Tulisan */}
+          <View style={{ gap: 20, paddingTop: 20, paddingBottom: 100 }}>
+            <View>
+              <Text style={styles.title}>Mesin Kopi Kamu Mengalami Kendala?</Text>
+              <Text style={styles.text}>
+                Silahkan beri tahu kami ciri-ciri yang mesin kopi kamu alami. Kami akan berusaha
+                membantu
+              </Text>
             </View>
 
-            {/* Tulisan */}
-            <View style={{ gap: 20, paddingTop: 20 }}>
-              <View>
-                <Text style={styles.title}>Mesin Kopi Kamu Mengalami Kendala?</Text>
+            {/* Jenis-Jenis Kerusakan */}
+            <View>
+              <Text style={styles.title}>Pilih Kendala</Text>
+              <TouchableOpacity style={styles.viewRow} onPress={() => setModalKendala(true)}>
                 <Text style={styles.text}>
-                  Silahkan beri tahu kami ciri-ciri yang mesin kopi kamu alami. Kami akan berusaha
-                  membantu
+                  Terdapat beberapa kerusakan yang sering terjadi. Klik untuk melihat selengkapnya.
                 </Text>
-              </View>
+                <Icon name="chevron-down" type="ionicon" color="#374151" size={20} />
+              </TouchableOpacity>
+            </View>
 
-              {/* Jenis-Jenis Kerusakan */}
-              <View>
-                <Text style={styles.title}>Pilih Kendala</Text>
-                <TouchableOpacity style={styles.viewRow} onPress={() => setModalKendala(true)}>
-                  <Text style={styles.text}>
-                    Terdapat beberapa kerusakan yang sering terjadi. Klik untuk melihat
-                    selengkapnya.
-                  </Text>
-                  <Icon name="chevron-down" type="ionicon" color="#374151" size={20} />
-                </TouchableOpacity>
-              </View>
-              {/* Ciri-Ciri Kerusakan */}
-              <View>
-                <Text style={styles.title}>Kerusakan Yang Diperkirakan</Text>
-                <TouchableOpacity style={styles.viewRow}>
-                  <Text style={styles.text}>
-                    Berdasarkan kendala yang kamu berikan, bisa kami simpulkan kemungkinkan kendala
-                    yang kamu alami adalah:
-                  </Text>
-                </TouchableOpacity>
+            {/* Ciri-Ciri Kerusakan */}
+            <View>
+              <Text style={styles.title}>Kerusakan Yang Diperkirakan</Text>
+
+              <Text style={styles.text}>
+                Berdasarkan kendala yang kamu berikan, bisa kami simpulkan kemungkinkan kendala yang
+                kamu alami adalah:
+              </Text>
+              <View style={styles.text}>
+                <Text style={styles.text}>Jenis Kerusakan:</Text>
+                {jenisKerusakan && jenisKerusakan.length > 0 ? (
+                  jenisKerusakan.map((kerusakan, index) => {
+                    return (
+                      <View key={index}>
+                        <Text style={styles.text}>
+                          {index + 1}. {kerusakan.diagnosis}
+                        </Text>
+                        <Text style={styles.text}>
+                          Persentase Kerusakan: {kerusakan.percentage}%
+                        </Text>
+                        {kerusakan.percentage === 100 && (
+                          <Text style={styles.text}>
+                            Kesimpulan: Mesin kopi rusak karena {kerusakan.diagnosis}
+                          </Text>
+                        )}
+                      </View>
+                    );
+                  })
+                ) : (
+                  <View>
+                    <Text style={styles.text}>1. Kerusakan belum bisa dipastikan</Text>
+                    <Text style={styles.text}>Persentase Kerusakan: 33%</Text>
+                  </View>
+                )}
               </View>
             </View>
-            <ModalKendala modalKendalaVisible={modalKendala} setModalKendala={setModalKendala} />
-          </ScrollView>
-        </View>
-      </Layout>
-    </>
+          </View>
+
+          <ModalKendala
+            modalKendalaVisible={modalKendala}
+            setModalKendala={setModalKendala}
+            handleKerusakan={handleKerusakan}
+          />
+        </ScrollView>
+      </View>
+    </Layout>
   );
 };
 
@@ -87,6 +118,14 @@ const styles = StyleSheet.create({
     paddingVertical: 2,
     textAlign: 'justify',
     flex: 1,
+  },
+  text1: {
+    fontFamily: 'Poppins_Regular',
+    paddingHorizontal: 10,
+    paddingVertical: 2,
+    textAlign: 'justify',
+    flex: 1,
+    marginBottom: 30,
   },
   title: {
     fontFamily: 'Poppins_Bold',
